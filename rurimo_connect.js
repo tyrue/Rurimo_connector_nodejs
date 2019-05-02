f326a85710fc5cab8a5fcdf51394667ec4afd410
tyrue <kdh9426@naver.com>
2019-05-03 00:57:13 +0900

    랭킹 css 조절

--- rurimo_connect.js
+++ rurimo_connect.js
@@ -1,6 +1,7 @@
 var express = require('express'); // pug 쓰기 위한 라이브러리
 var db = require('./public/mysql');
 var app = express();
+var day = require('./public/date');
 
 app.locals.pretty = true; // 브라우저에서 코드 볼때 예쁘게 해줌
 app.set('view engine', 'pug'); // 템플릿 엔진은 pug
@@ -22,17 +23,17 @@ app.get('/', function (req, res) // home 페이지
     });
 
 app.get('/ranking', function (req, res) // home 페이지
-{
-    db.query(`select job, nickname, LEVEL, maxhp, maxmp from ${user_table} order by (maxhp + maxmp) desc
-        `, function (error, ranker) {
-        if (error) {
-            throw error;
-        }
-        res.render('rank', {
-            time: Date(),
-            title: '흑바온 Ranking!',
-            data: ranker
-
-        }); // 템플릿을 가져와서 적용한다는 뜻
+    {
+        db.query(`select job, nickname, LEVEL, maxhp, maxmp from ${user_table} order by (maxhp + maxmp) desc`, function (error, result) {
+            if (error) {
+                throw error;
+            }
+            day = new Date();
+            day = day.format("yyyy-MM-dd a/p hh:mm:ss");
+            res.render('rank', {
+                time: day,
+                title: '[ 흑바온 명예의 전당 ]',
+                data: result
+            }); // 템플릿을 가져와서 적용한다는 뜻
+        });
     });
-});
