5b4e8da839096ebc58355d6414b43f6b2ad9de72
tyrue@github.com <kdh9426@naver.com>
2023-05-31 22:46:22 +0900

    저장용

--- rurimo_connect.js
+++ rurimo_connect.js
@@ -24,7 +24,12 @@ app.get('/', function (req, res) // home 페이지
 
 app.get('/ranking', function (req, res) // home 페이지
 {
-    db.query(`select job, nickname, LEVEL, maxhp, maxmp from ${user_table} order by (maxhp + maxmp) desc`, function (error, result) {
+    db.query(`
+    select job, nickname, LEVEL, maxhp, maxmp 
+    from ${user_table} 
+    where id != 'anwjr'
+    order by (maxhp + maxmp) desc
+    `, function (error, result) {
         if (error) {
             throw error;
         }
